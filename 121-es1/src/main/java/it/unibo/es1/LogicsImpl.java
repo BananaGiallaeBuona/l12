5924dd8819a309d48c9446a2aaf2551da1706d2e
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-09 16:28:05 +0100

    i'v adapted the code, but the value can be bigger thant value

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
  * Implementation of the Logics interface.
  */
 public class LogicsImpl implements Logics {
-    private final List<Button> buttons;
+    private final List<Integer> buttons;
 
     /**
      * Constructor.
@@ -17,7 +17,7 @@ public class LogicsImpl implements Logics {
     public LogicsImpl(final int size) {
         this.buttons = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
-            this.buttons.add(new Button());
+            this.buttons.add(0);
         }
     }
 
@@ -34,9 +34,7 @@ public class LogicsImpl implements Logics {
      */
     @Override
     public List<Integer> values() {
-        final List<Integer> values = new ArrayList<>();
-        buttons.stream().forEach(i -> values.add(i.getValue()));
-        return values;
+        return buttons;
     }
 
     /**
@@ -45,7 +43,7 @@ public class LogicsImpl implements Logics {
     @Override
     public List<Boolean> enabledStates() {
         final List<Boolean> states = new ArrayList<>();
-        buttons.stream().forEach(i -> states.add(i.getValue() != buttons.size()));
+        buttons.stream().forEach(i -> states.add(i != buttons.size()));
         return states;
     }
 
@@ -54,9 +52,8 @@ public class LogicsImpl implements Logics {
      */
     @Override
     public int hit(final int elem) {
-        final Button but = buttons.get(elem);
-        but.increment();
-        return but.getValue();
+        buttons.add(elem, buttons.get(elem)+1);
+        return buttons.get(elem);
     }
 
     /**
@@ -66,17 +63,17 @@ public class LogicsImpl implements Logics {
     public String result() {
         //sample: <<0|2|1|3>>
         final StringBuilder result = new StringBuilder("<<");
-        for (final Button button : buttons) {
-            result.append(Integer.toString(button.getValue())).append('|'); 
+        for (final Integer button : buttons) {
+            result.append(button).append('|'); 
         }
         result.replace(result.lastIndexOf("|"), result.length(), ">>");
         return result.toString();
     }
 
     private boolean checkEquals() {
-        final int value = buttons.get(0).getValue(); // we have the 1st, then we 
-        for (final Button button : buttons) {
-            if (value == 0 || value != button.getValue()) {
+        final int value = buttons.get(0); // we have the 1st, then we 
+        for (final Integer button : buttons) {
+            if (value == 0 || value != button) {
                 return false;
             }
         }
