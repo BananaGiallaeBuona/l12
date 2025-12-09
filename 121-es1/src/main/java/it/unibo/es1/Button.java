cfabbc6bbea3e6040ea904fe31205255447ee67b
Nikita Piraino <nikita.piraino@studio.unibo.it>
2025-12-09 15:51:05 +0100

    I've talked to the tutpr and 've undorstood that taking a field that consider if abutton is actionable or not can be calcualted in th emoment

--- 121-es1/src/main/java/it/unibo/es1/Button.java
+++ 121-es1/src/main/java/it/unibo/es1/Button.java
@@ -1,6 +1,9 @@
 package it.unibo.es1;
 
-public class Button {
+/**
+ * a model implementation of the button.
+ */
+public final class Button {
     private int value;
     private boolean state;
 
@@ -9,18 +12,30 @@ public class Button {
         state = true;
     }
 
+    /**
+     * @return the int value of the button
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * increments the value associatd to the button.
+     */
     public void increment() {
         this.value = value + 1;
     }
 
+    /**
+     * @return a variable that is true if the values is modifible
+     */
     public boolean isState() {
         return state;
     }
 
+    /**
+     * @param enabled it's used to modify the state of the button
+     */
     public void setEnabled(final boolean enabled) {
         this.state = enabled;
     }
